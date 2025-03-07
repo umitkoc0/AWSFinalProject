03d07a18b35d38acb739fd52081f4eec99cf3907
Umit KOC <umitkooc0@gmail.com>
2025-03-07 23:10:23 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-git clone https://github.com/xxxxxxxxxxxxx/xxxxxxxxxxxxxxx.git
-cd /home/ubuntu/xxxxxxxxxxxxxx/BlogDjango
+git clone https://github.com/umitkoc0/AWSFinalProject.git
+cd /home/ubuntu/AWSFinalProject/BlogDjango
 apt install python3-pip -y
 apt-get install python3.8-dev default-libmysqlclient-dev -y
 apt-get install libjpeg-dev zlib1g-dev -y -qq
 pip3 install -r requirements.txt
-cd /home/ubuntu/xxxxxxxxxxxxxxxxx/BlogDjango/src
+cd /home/ubuntu/AWSFinalProject/BlogDjango/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
